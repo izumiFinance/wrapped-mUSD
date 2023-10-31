2bc126d2042ffd5d74b6012ef725adb3f680e8cf
opcc <opcc22059@gmail.com>
2023-10-31 16:13:38 +0800

    add some notes to contract source code

--- contracts/WrapMUSD.sol
+++ contracts/WrapMUSD.sol
@@ -2,17 +2,32 @@
 
 pragma solidity ^0.8.19;
 
+/**
+ * @dev interfaces of MUSD
+ */
 interface IMUSD {
     function sharesOf(address _account) external view returns (uint256);
+
+    /**
+     * @dev get share amount from amount of mUSD
+     */
     function getSharesByRUSDY(
         uint256 _rUSDYAmount
     ) external view returns (uint256);
+
     function decimals() external view returns (uint8);
+
+    /**
+     * @dev transfer `amount` of mUSD
+     */
     function transferFrom(
         address _sender,
         address _recipient,
         uint256 _amount
     ) external returns (bool);
+    /**
+     * @dev transfer shares of mUSD directly
+     */
     function transferShares(
         address _recipient,
         uint256 _sharesAmount
@@ -20,24 +35,83 @@ interface IMUSD {
 }
 
 contract WrapMUSD {
+    /**
+     * name of this token
+     */
     string public name;
+    /**
+     * symbol of this token
+     */
     string public symbol;
+    /**
+     * decimals of this token, same as MUSD
+     */
     uint8 public decimals;
-
+    /**
+     * address of musd token
+     */
     address public mUSD;
 
+    /**
+     * @dev approve operation of this wrap token
+     * @param src owner of token
+     * @param guy operator
+     * @param val amount of mUSD
+     */
     event Approval(address indexed src, address indexed guy, uint256 val);
+
+    /**
+     * @dev transfer this wrap token
+     * @param src source address
+     * @param dst destination address
+     * @param val amount of mUSD
+     */
     event Transfer(address indexed src, address indexed dst, uint256 val);
+
+    /**
+     * @dev deposit musd to this wrapped token
+     * @param dst destination address
+     * @param val amount of token wrapped token
+     */
     event Deposit(address indexed dst, uint256 val);
+
+    /**
+     * @dev withdraw musd from this wrap token
+     * @param src address to get withdrawed musd
+     * @param val amount of wrapped token
+     */
     event Withdrawal(address indexed src, uint256 val);
 
+    /**
+     * balance of account
+     */
     mapping(address => uint256) public balanceOf;
+
+    /**
+     * shares of account accumulated when deposit
+     */
     mapping(address => uint256) public sharesOf;
+    /**
+     * allowance[src][spender] is max amount of token for spender to transfer
+     * from src
+     */
     mapping(address => mapping(address => uint256)) public allowance;
+    /**
+     * amount of shares chargeReceiver can collect
+     */
     uint256 public chargeShares;
+    /**
+     * who can collect shares
+     */
     address public chargeReceiver;
+    /**
+     * total amount of token
+     */
     uint256 public totalSupply;
 
+    /**
+     * owner of this contract
+     */
     address private _owner;
 
     function _msgSender() internal view virtual returns (address) {
@@ -95,6 +169,13 @@ contract WrapMUSD {
         emit OwnershipTransferred(oldOwner, newOwner);
     }
 
+    /**
+     * @dev constructor of this contract, contract owner will be msg.sender
+     * @param _name, name of token
+     * @param _symbol, symbol of token
+     * @param _mUSD, address of musd
+     * @param _chargeReceiver, who collect charged shares
+     */
     constructor(
         string memory _name, 
         string memory _symbol, 
@@ -124,11 +205,16 @@ contract WrapMUSD {
             shareLocal = share * remain / balance * val / remain;
         }
     }
-
+    /**
+     * @dev change chargeReceiver, only owner can call this interface
+     */
     function changeChargeReceiver(address _chargeReceiver) external onlyOwner {
         chargeReceiver = _chargeReceiver;
     }
 
+    /**
+     * @dev collect charged shares, only chargeReceiver can call this interface
+     */
     function collectCharge() external {
         require(msg.sender == chargeReceiver, "not receiver");
         if (chargeShares > 0) {
@@ -137,6 +223,10 @@ contract WrapMUSD {
         }
     }
 
+    /**
+     * @dev deposit musd to this contract
+     * @param val of musd to deposit
+     */
     function deposit(uint256 val) external {
         require(val > 0, "val > 0");
         uint256 beforeTotShare = IMUSD(mUSD).sharesOf(address(this));
@@ -150,12 +240,19 @@ contract WrapMUSD {
         emit Deposit(msg.sender, val);
     }
 
+    /**
+     * @dev withdraw wrapped musd from this contract
+     * @param val of musd to deposit
+     * @param recipient address to receive withdrawed token
+     */
     function withdraw(uint256 val, address recipient) external {
         if (val > 0) {
             uint256 balance = balanceOf[msg.sender];
             uint256 shares = sharesOf[msg.sender];
             require(balance >= val);
+            // compute delta shares by current price
             uint256 dShares = IMUSD(mUSD).getSharesByRUSDY(val);
+            // compute delta shares by share/balance rate
             uint256 dSharesLocal = getShareLocal(dShares, balance, val);
             if (dShares < dSharesLocal) {
                 IMUSD(mUSD).transferShares(recipient, dShares);
@@ -200,7 +297,9 @@ contract WrapMUSD {
             balanceDst.balance = balanceOf[dst];
             balanceDst.shares = sharesOf[dst];
 
+            // compute delta shares by current price
             uint256 dShares = IMUSD(mUSD).getSharesByRUSDY(val);
+            // compute delta shares by share/balance rate
             uint256 dSharesLocal = getShareLocal(balanceSrc.shares, balanceSrc.balance, val);
 
             if (dShares < dSharesLocal) {
@@ -228,18 +327,34 @@ contract WrapMUSD {
             sharesOf[dst] = balanceDst.balance;
         }
     }
-    function approve(address guy, uint256 val) external returns (bool) {
-        allowance[msg.sender][guy] = val;
+    /**
+     * @dev approve a spender to spend msg.sender's some amount of wrapped token
+     * @param spender address of spender
+     * @param val amount of wrapped musd
+     */
+    function approve(address spender, uint256 val) external returns (bool) {
+        allowance[msg.sender][spender] = val;
 
-        emit Approval(msg.sender, guy, val);
+        emit Approval(msg.sender, spender, val);
 
         return true;
     }
 
+    /**
+     * @dev transfer some wrapped musd to dst from msg.sender
+     * @param dst, destination address
+     * @param val, amount of wrapped musd
+     */
     function transfer(address dst, uint256 val) external returns (bool) {
         return transferFrom(msg.sender, dst, val);
     }
 
+    /**
+     * @dev transfer some wrapped musd to dst from src
+     * @param src, source address
+     * @param dst, destination address
+     * @param val, amount of wrapped musd
+     */
     function transferFrom(
         address src,
         address dst,
